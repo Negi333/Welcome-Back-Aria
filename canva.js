429db83bdf0e463862c0f583c8521045cb9a9abf
Negi333 <2556boss@gmail.com>
2024-10-12 17:19:02 +0700

    Negi

---
+++ canva.js
@@ -0,0 +1,8 @@
+// Function to toggle animation
+function toggleAnimation() {
+    var card = document.getElementById("animated-card");
+    card.classList.toggle("paused");
+}
+
+// Example: Call this function to toggle animation on button click
+// document.getElementById("toggle-button").addEventListener("click", toggleAnimation);
